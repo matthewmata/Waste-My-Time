674e82056fc60d8f6992444e5c6c5b53034e291e
Hack Reactor Student <student@hackreactor.com>
2019-03-24 18:09:41 -0700

    .

--- helpers/youtube.js
+++ helpers/youtube.js
@@ -1,28 +1,48 @@
 const axios = require('axios')
 const API_key = require('./config.js');
 
-// let getBbsFallingVideo = () => {
-// 	return axios
-// 		.get('https://www.googleapis.com/youtube/v3/search', {
-// 			data: {
-// 				'maxResults': 1,
-// 				'part': 'snippet',
-// 				'q': 'Babies falling down',
-// 				'type': 'video',
-// 				'key': API_key
-// 			}
-// 		})
-// 		.then(data => console.log(data))
-// 		.catch(err => console.error(err))
+let getBbsFallingVideo = () => {
+	return axios
+		.get('https://www.googleapis.com/youtube/v3/search', {
+			headers: {
+        key: API_key.key,
+        part: 'snippet',
+        q: 'cats',
+        maxResults: 5,
+        type: 'video',
+        videoEmbeddable: 'true'
+      }
+		})
+		.then(data => console.log(data))
+		.catch(err => console.error(err))
 
-// }
+}
 
-// getBbsFallingVideo();
+module.exports = getBbsFallingVideo
+getBbsFallingVideo();
 
-// const getReposByUsername = ()=> axios
+let getBbsFallingVideo = () => {
+	return axios
+		.get('https://www.googleapis.com/youtube/v3/search', {
+			headers: {
+        key: API_key.key,
+        part: 'snippet',
+        q: 'cats',
+        maxResults: 5,
+        type: 'video',
+        videoEmbeddable: 'true'
+      }
+		})
+		.then(data => console.log(data))
+		.catch(err => console.error(err))
+}
+
+
+// const getReposByUsername = () => axios
 //   .get(`https://api.github.com/users/matthewmata/repos`, {
 //     headers: { Authorization: 'fba60a2c8a79f283a4dbf907a62f60f2e6aedfad' },
-//   }).then(data => console.log(data));
+//   })
+//   .then(data => console.log(data));
 
 // module.exports.getReposByUsername = getReposByUsername;
 // getReposByUsername();
